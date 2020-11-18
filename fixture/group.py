194c3d28a4256d36669d29975651d32b841203c3
OlgaBrozhe <olya_lek@yahoo.com>
2020-11-18 15:51:14 -0800

    Implemented test to delete a group. Test runs successfully.

--- fixture/group.py
+++ fixture/group.py
@@ -1,10 +1,9 @@
-from model.group_form import GroupForm
-
-
 class GroupHelper:
 
     def __init__(self, app):
         self.app = app
+        self.group_editor = self.app.application.window(title="Group editor")
+        self.delete_group_editor = self.app.application.window(title="Delete group")
 
     def get_groups_list(self):
         self.open_group_editor()
@@ -17,16 +16,26 @@ class GroupHelper:
 
     def open_group_editor(self):
         self.app.main_window.window(auto_id="groupButton").click()
-        self.group_editor = self.app.application.window(title="Group editor")
         self.group_editor.wait("visible")
 
     def close_group_editor(self):
-        pass
+        self.group_editor.close()
 
     def add_new_group(self, group):
         self.open_group_editor()
         self.group_editor.window(auto_id="uxNewAddressButton").click()
-        input = self.group_editor.window(class_name="Edit")
-        input.set_text(group)
-        input.type_keys("\n")
+        input_group_name = self.group_editor.window(class_name="Edit")
+        input_group_name.set_text(group)
+        input_group_name.type_keys("\n")
+        self.close_group_editor()
+
+    def del_group(self, group):
+        self.open_group_editor()
+        # Select a group in the tree
+        tree = self.group_editor.window(auto_id="uxAddressTreeView")
+        root = tree.tree_root().text()
+        tree.get_item(("\{}\{}").format(root, group)).click_input()
+        self.group_editor.window(auto_id="uxDeleteAddressButton").click()
+        self.delete_group_editor.wait("visible")
+        self.delete_group_editor.window(auto_id="uxOKAddressButton").click()
         self.close_group_editor()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,8 +1,9 @@
 def test_add_group(app):
     old_groups_list = app.group.get_groups_list()
-    app.group.add_new_group("TestGroup")
+    group = "TestGroup"
+    app.group.add_new_group(group)
     new_groups_list = app.group.get_groups_list()
     # Append old list with new item, clear and sort lists ascending and check if they are still equal
-    old_groups_list.append("TestGroup")
+    old_groups_list.append(group)
     # Check match of UI and DB lists
     assert sorted(old_groups_list) == sorted(new_groups_list)
